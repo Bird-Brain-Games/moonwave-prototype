66608bd4ef5b24d768058d0135e2f2a63f651838
Graham Watson <12705535+Goombario@users.noreply.github.com>
2017-10-03 21:42:02 -0400

    Added timer

---
+++ Assets/MoonShot/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    Text m_Text;
+    public float m_Time;
+
+	// Use this for initialization
+	void Start () {
+        m_Text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Reduce the timer
+        m_Time -= Time.deltaTime;
+
+        // Update the timer text
+        if (((int)m_Time) % 60 < 10)
+            m_Text.text = (((int)m_Time) / 60) + ":0" + (((int)m_Time) % 60);
+        else
+            m_Text.text = (((int)m_Time) / 60) + ":" + (((int)m_Time) % 60);
+    }
+}
