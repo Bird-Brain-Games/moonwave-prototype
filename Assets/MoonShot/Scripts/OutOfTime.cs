d05b17c1916387ec8ce1ff79a1d971fb60958705
Graham Watson <12705535+Goombario@users.noreply.github.com>
2017-10-12 14:58:40 -0400

    Fixed merge issues

---
+++ Assets/MoonShot/Scripts/OutOfTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutOfTime : MonoBehaviour {
+
+    public Text timerText;
+    Text m_Text;
+    Timer m_Timer;
+
+	// Use this for initialization
+	void Start () {
+        m_Timer = timerText.GetComponent<Timer>();
+        m_Text = GetComponent<Text>();
+        m_Text.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_Timer.OutOfTime() && !m_Text.enabled)
+        {
+            m_Text.enabled = true;
+
+        }
+    }
+}
